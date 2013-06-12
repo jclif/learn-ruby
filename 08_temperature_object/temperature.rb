21d66181a2aac2664eaecd76de2b603edf2129f1
Jared Clifton <jaredcraigclifton@gmail.com>
2013-06-13 03:49:41 +0800

    finished 8, except for bonus. wasn't sure what it meant

--- 08_temperature_object/temperature.rb
+++ 08_temperature_object/temperature.rb
@@ -1,2 +1,41 @@
 class Temperature
+  def initialize(opts={})
+    @options = opts
+  end
+
+  def in_fahrenheit
+    if @options[:f]
+      return @options[:f]
+    else
+      return ((@options[:c] * (9.0 / 5.0)) + 32)
+    end
+  end
+
+  def in_celsius
+    if @options[:c]
+      return @options[:c]
+    else
+      return ((@options[:f] - 32) * (5.0 / 9.0))
+    end
+  end
+
+  def self.from_celsius(c)
+    Temperature.new(:c => c)
+  end
+
+  def self.from_fahrenheit(f)
+    Temperature.new(:f => f)
+  end
+end
+
+class Celsius < Temperature
+  def initialize(c)
+    super(:c => c)
+  end
+end
+
+class Fahrenheit < Temperature
+  def initialize(f)
+    super(:f => f)
+  end
 end
