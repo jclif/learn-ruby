2f0dcd5b8885fa2a42641d2684e39fe56253d974
Jared Clifton <jaredcraigclifton@gmail.com>
2013-07-30 21:56:18 -0500

    still workign

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -1,10 +1,29 @@
 class XmlDocument
-  def hello(hash={})
-    str = ""
-    hash.each {|key,val|
-      str << %Q( #{key.to_s}='#{val}')
-    }      
-    "<hello#{str}/>"
+  def initialize(nested = false)
+    @nested = nested
   end
-  def missing_method
+  def hello(hash={})
+    if block_given?
+      %Q(<hello>#{yield}</hello>)
+    else
+      str = ""
+      hash.each {|key,val|
+        str << %Q( #{key.to_s}='#{val}')
+      }       
+      "<hello#{str}/>"
+    end
+  end
+  def method_missing(tag_name,hash={})
+    if block_given?
+      %Q(<#{tag_name}>#{yield}</#{tag_name}>)
+    elsif hash == {}
+      "<#{tag_name}/>"
+    else
+      str = ""
+      hash.each {|key,val|
+        str << %Q( #{key.to_s}='#{val}')
+      }
+      "<#{tag_name}#{str}/>"
+    end
+  end    
 end
