60215bfc2ea568c176d7751aa016073abdb07062
Jared Clifton <jclif155@gmail.com>
2013-07-31 04:13:11 -0500

    not sure why this approach for 13 isn't working. will try another

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -1,21 +1,22 @@
 class XmlDocument
-  def initialize(nested = false)
-    @nested = nested
-  end
-  def hello(hash={})
-    if block_given?
-      %Q(<hello>#{yield}</hello>)
-    else
-      str = ""
-      hash.each {|key,val|
-        str << %Q( #{key.to_s}='#{val}')
-      }       
-      "<hello#{str}/>"
-    end
+  def initialize(indent = false)
+    @indent = indent
+    @format = "\n"
+
   end
   def method_missing(tag_name,hash={})
     if block_given?
-      %Q(<#{tag_name}>#{yield}</#{tag_name}>)
+      if @indent
+        puts "info:"
+        p "format: [#{@format}]"
+        puts "tag: #{tag_name}"
+      end
+      format = @format
+      @format << "  "
+      y = yield
+      s = %Q(<#{tag_name}>#{format if @indent}#{y}</#{tag_name}>#{format if @indent})
+      p "s is [#{s}]"
+      return s
     elsif hash == {}
       "<#{tag_name}/>"
     else
@@ -23,7 +24,7 @@ class XmlDocument
       hash.each {|key,val|
         str << %Q( #{key.to_s}='#{val}')
       }
-      "<#{tag_name}#{str}/>"
+      "<#{tag_name}#{str}/>#{"\n" if @indent}"
     end
   end    
 end
