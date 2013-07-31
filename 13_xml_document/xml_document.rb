03c91c8f184ebd4f372593ed6e8fc414d357bd14
Jared Clifton <jclif155@gmail.com>
2013-07-31 06:35:31 -0500

    error was caused by strange behavior with #{@format}, where it would not output the same string as a puts @format even though it was right before it

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -1,22 +1,19 @@
 class XmlDocument
   def initialize(indent = false)
     @indent = indent
-    @format = "\n"
-
+    @f = "\n"
+    @first = true
   end
-  def method_missing(tag_name,hash={})
+  def method_missing(tag_name,hash={},&block)
     if block_given?
       if @indent
-        puts "info:"
-        p "format: [#{@format}]"
-        puts "tag: #{tag_name}"
+        first = @first
+        @first = false
+        @f << "  "
+        return "<#{tag_name}>" + @f + yield + "</#{tag_name}>" + "#{@f=@f[0...-2];if not first then @f else "\n" end}"
+      else
+        return %Q(<#{tag_name}>#{yield}</#{tag_name}>)
       end
-      format = @format
-      @format << "  "
-      y = yield
-      s = %Q(<#{tag_name}>#{format if @indent}#{y}</#{tag_name}>#{format if @indent})
-      p "s is [#{s}]"
-      return s
     elsif hash == {}
       "<#{tag_name}/>"
     else
@@ -24,7 +21,7 @@ class XmlDocument
       hash.each {|key,val|
         str << %Q( #{key.to_s}='#{val}')
       }
-      "<#{tag_name}#{str}/>#{"\n" if @indent}"
+      "<#{tag_name}#{str}/>#{@f=@f[0...-2];@f if @indent}"
     end
   end    
 end
